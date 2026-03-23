ec2befd9a513f82b48746888f65c2cf1d212a60b
Etienne Verwant <efverwant@gmail.com>
2026-03-23 18:58:23 +0200

    mobile-ability

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let btnMobileMenu = $("#btnMobileMenu");
+let menuItems = $(".menuItem");
 
-// Write your JavaScript code.
+btnMobileMenu.click(()=>{
+    menuItems.toggle();
+    // $("#btnMpbileMenu li button img")
+})
+
